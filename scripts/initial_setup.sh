d34a36bb5f40a85814da12d2d5edd9b5ef984887
B Shiva Kumar <92181068+B-Shiva-Kumar@users.noreply.github.com>
2023-02-17 10:32:02 +0530

    updated changes

--- scripts/initial_setup.sh
+++ scripts/initial_setup.sh
@@ -6,25 +6,25 @@ sudo apt-get update -y
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
 
 # install terraform 
-# sudo apt update && sudo apt install gpg
-# wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
-# gpg --no-default-keyring --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg --fingerprint
-# echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-# sudo apt update
-# sudo apt install terraform
+sudo apt update && sudo apt install gpg
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+gpg --no-default-keyring --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg --fingerprint
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update
+sudo apt install terraform
 
 # or
 
-snap install terraform --classic
+# snap install terraform --classic
 
-# install awscli
-# sudo apt install awscli -y
-snap install aws-cli --classic
+install awscli
+sudo apt install awscli -y
+# snap install aws-cli --classic
 
 # install docker
-# sudo apt-get update
-# sudo apt install docker.io -y
-# # docker --version
-# sudo apt-get update
-# sudo apt install docker-compose -y
-# # docker-compose --version
+sudo apt-get update
+sudo apt install docker.io -y
+# docker --version
+sudo apt-get update
+sudo apt install docker-compose -y
+# docker-compose --version
